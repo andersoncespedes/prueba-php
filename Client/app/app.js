a047f9be7ee632026b7efaa36431853c7b6df62f
anderson <andersontrabajos2@gmail.com>
2024-02-21 08:54:02 -0500

    feat(Client): :sparkles: Creation of controllers and services in angularjs with datable

--- Client/app/app.js
+++ Client/app/app.js
@@ -1,40 +1,104 @@
-var app = angular.module('myApp', [])
-.constant('API_URL', 'http://localhost/prueba-tecnica/API/index.php/')
-.controller('ProductController', ['$scope', 'ProductService', function($scope, ProductService) {
-    // Get all products
-    ProductService.getProducts().then(function(response) {
+var app = angular
+  .module("myApp", [])
+  .constant("API_URL", "http://localhost/prueba-tecnica/API/index.php/")
+  //Controller
+  .controller("ProductController", [
+    "$scope",
+    "ProductService",
+    "$window",
+    function ($scope, ProductService, $window) {
+     
+   
+      //declaration of variables
+      $scope.productDataUp = {};
+      $scope.productData = {};
+      // Get all products
+      ProductService.getProducts().then(function (response) {
+     // datatable
+        $(document).ready(function() {
+            $('#table').DataTable();
+        });
         $scope.products = response.data;
-    });
-    // Get One Product
-    $scope.getOne = function (code){
-        ProductService.getOneProduct(code).then(function(response) {
+      });
+      // Get One Product
+      $scope.getOne = function (code) {
+        ProductService.getOneProduct(code)
+          .then(function (response) {
             $scope.productDataUp = {
-                Name: response.data.Name,
-                Price: response.data.Price,
-                Category: response.data.Category
+              Name: response.data.Name,
+              Code: response.data.Code,
+              Price: response.data.Price,
+              Category: response.data.Category,
             };
-        }).catch(function(error) {
+          })
+          .catch(function (error) {
             console.error(error);
-        });
-    }
-    // Save Product
-    $scope.productData = {};
-    $scope.saveProduct = function() {
-        ProductService.saveProduct( $scope.productData).then(function(response) {
+
+          });
+      };
+      // Save Product
+      $scope.saveProduct = function () {
+        ProductService.saveProduct($scope.productData)
+          .then(function (response) {
             console.log(response.data);
-        }).catch(function(error) {
+            $window.location.reload();
+          })
+          .catch(function (error) {
             console.error(error);
-        });
-    };
-}])
-.service('ProductService', ['$http', 'API_URL', function($http, API_URL) {
-    this.getProducts = function() {
+            
+          });
+      };
+      //Update Product
+      $scope.updateProduct = function () {
+        ProductService.updateProduct(
+          $scope.productDataUp,
+          $scope.productDataUp.Code
+        )
+          .then(function (response) {
+            console.log(response.data);
+            $window.location.reload();
+          })
+          .catch(function (error) {
+            console.error(error);
+          });
+      };
+      // delete product
+      $scope.deleteProduct = function (code) {
+        let confirm = $window.confirm(
+          "Are you sure you want to delete this Product"
+        );
+        if (confirm) {
+          ProductService.deleteProduct(code)
+            .then(function (response) {
+              console.log(response.data);
+                $window.location.reload();
+            })
+            .catch(function (error) {
+              console.error(error);
+            });
+        }
+      };
+    },
+  ])
+  // Services
+  .service("ProductService", [
+    "$http",
+    "API_URL",
+    function ($http, API_URL) {
+      this.getProducts = function () {
         return $http.get(`${API_URL}product/show`);
-    };
-    this.getOneProduct = function(code) {
+      };
+      this.getOneProduct = function (code) {
         return $http.get(`${API_URL}product/showOne?code=${code}`);
-    };
-    this.saveProduct = function(data){
-        return $http.post(`${API_URL}product/create`,data);
-    }
-}]);
+      };
+      this.saveProduct = function (data) {
+        return $http.post(`${API_URL}product/create`, data);
+      };
+      this.updateProduct = function (data, code) {
+        return $http.put(`${API_URL}product/update?code=${code}`, data);
+      };
+      this.deleteProduct = function (code) {
+        return $http.delete(`${API_URL}product/delete?code=${code}`);
+      };
+    },
+  ]);
